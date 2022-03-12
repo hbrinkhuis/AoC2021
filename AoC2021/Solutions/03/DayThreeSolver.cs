b51d9dd24f8a3a3554d004d26012ba2640f73711
Henri Brinkhuis <henri@htbr.nl>
2022-03-12 21:42:20 +0100

    Day Three solved

--- AoC2021/Solutions/03/DayThreeSolver.cs
+++ AoC2021/Solutions/03/DayThreeSolver.cs
@@ -13,33 +13,35 @@ public class DayThreeSolver : AbstractSolver, ISolver
     {
         // assuming equal length lines
         var lineLength = Lines[0].Length;
-        var counts = new int[lineLength];
+        var mostOccurring = new int[lineLength];
 
         foreach (var line in Lines)
         {
-            foreach (var (i,digit) in line.ToCharArray().Select((c, i) => (i, c)))
+            foreach (var (i, digit) in line.ToCharArray().Select((c, i) => (i, c)))
             {
                 switch (digit)
                 {
                     case '1':
-                        counts[i]++;
+                        mostOccurring[i]++;
                         break;
                     case '0':
-                        counts[i]--;
+                        mostOccurring[i]--;
                         break;
                 }
             }
         }
+
+        var counts = mostOccurring.Select(c => c > 1);
         var gammaRate = uint.MinValue;
         var epsilonRate = uint.MinValue;
 
         foreach (var (i, c) in counts.Reverse().Select((c, i) => (i, c)))
         {
             var mask = (uint)1 << i;
-            if (c <= 0)
-                epsilonRate |= mask;
-            else
+            if (c)
                 gammaRate |= mask;
+            else
+                epsilonRate |= mask;
         }
 
         return (gammaRate * epsilonRate).ToString();
@@ -47,8 +49,36 @@ public class DayThreeSolver : AbstractSolver, ISolver
 
     public string SolvePartTwo()
     {
-        return string.Empty;
+        char OxygenMatcher(int val) => val >= 0 ? '1' : '0';
+        char Co2Matcher(int val) => val >= 0 ? '0' : '1';
+        var oxygenGeneratorValue = MatchLinesRecursive(Lines.ToArray(), OxygenMatcher);
+        var co2ScrubberValue = MatchLinesRecursive(Lines.ToArray(), Co2Matcher);
+        var oxygenGenerator = Convert.ToInt32(oxygenGeneratorValue[0], 2);
+        var co2Scrubber = Convert.ToInt32(co2ScrubberValue[0], 2);
+        return $"{oxygenGenerator * co2Scrubber}";
     }
 
+    private static string[] MatchLinesRecursive(string[] remainingReport, Func<int, char> matcher, int characterPosition = 0)
+    {
+        var counter = 0;
+        foreach (var line in remainingReport)
+        {
+            switch (line[characterPosition])
+            {
+                case '0':
+                    counter--;
+                    break;
+                case '1':
+                    counter++;
+                    break;
+            }
+        }
+
+        var matchChar = matcher(counter); 
+        var matches = remainingReport.Where(c => c[characterPosition] == matchChar).ToArray();
+
+        return matches.Length > 1 ? MatchLinesRecursive(matches, matcher, ++characterPosition) : matches;
+    }
+    
     public int Day => 3;
 }
--- Aoc2021.Tests/DayThreeSolverTests.cs
+++ Aoc2021.Tests/DayThreeSolverTests.cs
@@ -1,3 +1,4 @@
+using AoC2021.Solutions;
 using Xunit;
 using AoC2021.Solutions._03;
 using FluentAssertions;
@@ -6,11 +7,11 @@ namespace Aoc2021.Tests;
 
 public class DayThreeSolverTests
 {
-    [Fact]
-    public void ExampleTest()
+    private readonly ISolver _sut;
+
+    public DayThreeSolverTests()
     {
-        string[] lines =
-        {
+        string[] lines = {
             "00100",
             "11110",
             "10110",
@@ -25,10 +26,21 @@ public class DayThreeSolverTests
             "01010"
         };
 
-        var sut = new DayThreeSolver(lines);
-
-        var result = sut.SolvePartOne();
+        _sut = new DayThreeSolver(lines);
+    }
+    [Fact]
+    public void PartOneTest()
+    {
+        var result = _sut.SolvePartOne();
 
         result.Should().Be("198");
+    }
+
+    [Fact]
+    public void PartTwoTest()
+    {
+        var result = _sut.SolvePartTwo();
+
+        result.Should().Be("230");
     }
 }
